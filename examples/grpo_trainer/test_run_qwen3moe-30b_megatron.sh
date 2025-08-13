25afcf3a456eaef2a60032011531a7efa6c1cf8a
lilei <lileill@shopee.com>
2025-08-13 16:51:02 +0800

    test

--- examples/grpo_trainer/test_run_qwen3moe-30b_megatron.sh
+++ examples/grpo_trainer/test_run_qwen3moe-30b_megatron.sh
@@ -37,7 +37,6 @@ python3 -m verl.trainer.main_ppo --config-path=config \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
     actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.rollout.top_k=1 \
-    actor_rollout_ref.ref.use_dynamic_bsz=True \
     actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=1 \
     actor_rollout_ref.ref.megatron.tensor_model_parallel_size=4 \
     actor_rollout_ref.ref.megatron.expert_model_parallel_size=8 \
