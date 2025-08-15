45ab0a2d76a220729dea8bde20598e5fb8d9c1fb
lilei <lileill@shopee.com>
2025-08-15 16:55:36 +0800

    test

--- verl/workers/sharding_manager/megatron_sglang.py
+++ verl/workers/sharding_manager/megatron_sglang.py
@@ -175,7 +175,8 @@ class MegatronSGLangShardingManager(BaseShardingManager):
                 self.transformer_config,
                 self.layer_name_mapping,
             )
-        await self.update_weights(per_tensor_param)
+        with simple_timer("update_weights", self.timing):
+            await self.update_weights(per_tensor_param)
         if self.offload_param:
             offload_megatron_model_to_cpu(self.actor_module)
         get_torch_device().empty_cache()
