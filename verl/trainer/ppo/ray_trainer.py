162747892fd0fd95b0b6ef87fb36dbaa8e8b343b
lilei <lileill@shopee.com>
2025-08-27 17:00:33 +0800

    test

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -982,11 +982,7 @@ class RayPPOTrainer:
                             gen_batch_output = self.async_rollout_manager.generate_sequences(gen_batch)
                         timing_raw.update(gen_batch_output.meta_info["timing"])
                         gen_batch_output.meta_info.pop("timing", None)
-                    #
-                    self.actor_rollout_wg.dump_memory_snapshot(
-                        tag=f"post_update_step{self.global_steps}", sub_dir=f"step{self.global_steps}"
-                    )
-                    #
+
                     if self.config.algorithm.adv_estimator == AdvantageEstimator.REMAX:
                         if self.reward_fn is None:
                             raise ValueError("A reward_fn is required for REMAX advantage estimation.")
