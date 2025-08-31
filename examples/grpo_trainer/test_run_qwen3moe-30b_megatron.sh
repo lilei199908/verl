3602edeabfb41a45d5800d9783cd28f43cd56622
lilei <lileill@shopee.com>
2025-09-01 00:28:41 +0800

    test

--- examples/grpo_trainer/test_run_qwen3moe-30b_megatron.sh
+++ examples/grpo_trainer/test_run_qwen3moe-30b_megatron.sh
@@ -41,7 +41,6 @@ python3 -m verl.trainer.main_ppo --config-path=config \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=8 \
     actor_rollout_ref.rollout.top_k=1 \
-    actor_rollout_ref.rollout.enforce_eager=True \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=1 \
     actor_rollout_ref.ref.megatron.tensor_model_parallel_size=4 \
