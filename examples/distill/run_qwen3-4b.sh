dd850c95d2bdb86b3113421941858aaf8908f5a4
lilei <lileill@shopee.com>
2025-09-05 12:33:32 +0800

    test

--- examples/distill/run_qwen3-4b.sh
+++ examples/distill/run_qwen3-4b.sh
@@ -11,7 +11,7 @@ python3 -m verl.trainer.main_ppo \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path=/data1/lilei/Qwen3-4B \
-    +actor_rollout_ref.teacher_model.path=/data1/lilei/Qwen3-4B
+    +actor_rollout_ref.teacher_model.path=/data1/lilei/Qwen3-4B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=30 \
