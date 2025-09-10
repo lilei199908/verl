5b975adfb5f7686cffb499f057728d1a95d0b9a2
lilei <lileill@shopee.com>
2025-09-10 15:57:23 +0800

    test

--- verl/utils/profiler/profile.py
+++ verl/utils/profiler/profile.py
@@ -106,7 +106,7 @@ class Profiler:
         if self.prof is not None and not self.saved:
             if not os.path.exists(self.config.save_path):
                 os.makedirs(self.config.save_path)
-            save_file_name = f"/prof_start_{self.config.step_start}_end_{self.config.step_end}_rank_{self.rank}.json"
+            save_file_name = f"/prof_start_{self.tool_config.step_start}_end_{self.tool_config.step_end}_rank_{self.rank}.json"
             print(f"[Profiler] Saving trace to {self.config.save_path + save_file_name}")
             self.prof.export_chrome_trace(self.config.save_path + save_file_name)
             self.enable = False
